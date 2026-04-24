52c13705a5c193873f3660a62b67768ba7ebb768
sungmint <zpcp1212@gmail.com>
2026-04-24 23:01:21 +0900

    Delete _config.js

--- _config.js
+++
@@ -1,9 +0,0 @@
-var SM_CONFIG = {
-  apiKey:            "AIzaSyAN7K0-V9PPepqmcNq0qA1xg_Sr8n-xiu0",
-  authDomain:        "qna-sungmint.firebaseapp.com",
-  databaseURL:       "https://qna-sungmint-default-rtdb.firebaseio.com",
-  projectId:         "qna-sungmint",
-  storageBucket:     "qna-sungmint.firebasestorage.app",
-  messagingSenderId: "1050653821753",
-  appId:             "1:1050653821753:web:eb604160345bea4fd5b6e8"
-};
